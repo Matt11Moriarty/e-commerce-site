cf9520b2604870fb9addd7e75c5c6b7c1a9b3dbe
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-09-13 14:39:41 -0700

    removed comments

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,18 +1,12 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   const categoryData = await Category.findAll();
   return res.json(categoryData)
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id);
     if (!categoryData) {
@@ -26,7 +20,6 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json({message: `Category id:${categoryData.id} created.`})
@@ -36,14 +29,12 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id);
     if (!categoryData) {
       res.status(404).json({message: 'No category with this id!'});
       return;
     }
-
     const [changedRows] = await Category.update(req.body, {
       where: {
         id: req.params.id
@@ -53,7 +44,6 @@ router.put('/:id', async (req, res) => {
       res.status(404).json({message: 'No changes made'})
       return;
     }
-
     res.status(200).json({message: `Category name updated to ${req.body.category_name}`})
   } catch (err) {
     res.status(500).json(err)
@@ -61,7 +51,6 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
     const {category_name} = await Category.findByPk(req.params.id)
     const deletedRows = await Category.destroy({
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
 
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
       include: [{model: Product, through: ProductTag, as: 'tagged_products'}]
     });
-      // include: [{ model: Product, through: ProductTag , as: 'tagged_products' }]
     return res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
@@ -18,8 +14,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{model: Product, through: ProductTag, as: 'tagged_products'}]
@@ -34,7 +28,6 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
   try {
     const tagData = await Tag.create(req.body);
     return res.status(200).json({message: `Tag id ${tagData.id} created`})
@@ -44,7 +37,6 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
   try {
     const tagData = await Tag.findByPk(req.params.id);
     if (!tagData) {
@@ -67,13 +59,12 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
     try {
       const tagData = await Tag.findByPk(req.params.id);
       if (!tagData) {
-        res.status(404).json({message: 'No tag exists with this id!'})
+        return res.status(404).json({message: 'No tag exists with this id!'})
       }
-      const deletedRows = await Tag.destroy ({
+      await Tag.destroy ({
         where: {
           id: req.params.id
         }
